eea4daa46a0f59cf69cfad14db605443d7d1e83b
aalekseyev <alekseyev.anton@gmail.com>
2017-09-19 16:25:10 +0300

    gde moj jogurt?

--- script_convert_db.sh
+++ script_convert_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-cat worldcitiespop.txt | awk -F ',' '{ print "INSERT INTO \"order\"(id, name, lattitide, longtitude) VALUES (" count++ "," "\x27" $3 "\x27" "," "\x27" $6 "\x27" "," "\x27" $7 "\x27" ");" }' > ./output.sql
+cat worldcitiespop.txt | awk -F ',' '{ gsub(/\x27/, "", $3); print "INSERT INTO \"order\"(id, name, lattitide, longtitude) VALUES (" count++ "," "\x27" $3 "\x27" "," "\x27" $6 "\x27" "," "\x27" $7 "\x27" ");" }' > ./output.sql
 
 sed -i 's/[^[:print:]]//' ./output.sql
